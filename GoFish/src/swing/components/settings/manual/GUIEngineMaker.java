eaa0c4ef2b26dd7090e674433cb4e5784af8ad13
Adam Narkunski <airdong@gmail.com>
2013-04-22 11:44:39 +0300

    save me ;

---
+++ GoFish/src/swing/components/settings/manual/GUIEngineMaker.java
@@ -0,0 +1,15 @@
+package swing.components.settings.manual;
+
+import java.util.Collection;
+import swing.components.settings.manual.playerItem.PlayerItem;
+
+/**
+ *
+ * @author adamnark
+ */
+public class GUIEngineMaker {
+
+    private Collection<PlayerItem> playerItems;
+    
+    
+}
